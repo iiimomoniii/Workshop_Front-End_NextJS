e4b6c15f8ab8c764b51272e6049aeac63cc597ea
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2024-07-13 22:18:00 +0700

    Update
    test client side

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,21 @@
+//0. test client side
+"use client"
 import React from 'react'
 
 type Props = {}
 
-export default function Index({}: Props) {
+export default function Index({ }: Props) {
   return (
     <div>
       <h1 className='text-2xl font-bold'>Todos App</h1>
-      <form className='flex flex-col w-[300px] my-16'>
-        <input type="text" name="message" className='px-4 py-2 mb-3' placeholder='write your job...'/>
+      
+      <form  action={async (formData:FormData)=>{
+      
+
+        //1. test client side
+        alert("1234");
+      }} className='flex flex-col w-[300px] my-16'>
+        <input type="text" name="message" className='px-4 py-2 mb-3' placeholder='write your job...' />
         <button className='bg-blue-500 rounded px-4 py-2 text-white font-semibold'>Submit</button>
       </form>
     </div>
