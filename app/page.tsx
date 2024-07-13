3002ead247f81f0c54579b5fb14322867dee42a8
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2024-07-13 22:18:27 +0700

    Update
    test server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-//0. test client side
-"use client"
 import React from 'react'
 
 type Props = {}
@@ -10,10 +8,8 @@ export default function Index({ }: Props) {
       <h1 className='text-2xl font-bold'>Todos App</h1>
       
       <form  action={async (formData:FormData)=>{
-      
-
-        //1. test client side
-        alert("1234");
+        "use server" //{/* 0. test server side */}
+        console.log("Running at server"); // {/* 1. test server side */}
       }} className='flex flex-col w-[300px] my-16'>
         <input type="text" name="message" className='px-4 py-2 mb-3' placeholder='write your job...' />
         <button className='bg-blue-500 rounded px-4 py-2 text-white font-semibold'>Submit</button>
