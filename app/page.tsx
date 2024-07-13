07f1bb950c423e064fef7088c3808f3e2294ebc8
Natdanai <32894861+iiimomoniii@users.noreply.github.com>
2024-07-13 21:12:05 +0700

    Update
    create form submit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-import Image from "next/image";
+import React from 'react'
 
-export default function Home() {
+type Props = {}
+
+export default function Index({}: Props) {
   return (
-    <div>index</div>
-  );
+    <div>
+      <h1 className='text-2xl font-bold'>Todos App</h1>
+      <form className='flex flex-col w-[300px] my-16'>
+        <input type="text" name="message" className='px-4 py-2 mb-3' placeholder='write your job...'/>
+        <button className='bg-blue-500 rounded px-4 py-2 text-white font-semibold'>Submit</button>
+      </form>
+    </div>
+  )
 }
